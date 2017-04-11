851c38ee22bb2c292e8f790e4b6c6087e6399ce8
roi3579 <roi@h-MacBook-Pro-sl-roi.local>
2017-04-12 00:05:05 +0300

    configure app to go index.html using default templates

--- NaamanServer/NaamanServer/urls.py
+++ NaamanServer/NaamanServer/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from home import views
+#
+# urlpatterns = [
+#     url(r'^admin/', admin.site.urls),
+# ]
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-]
+    # url(r'^api/', views.api, name='api'),
+    url(r'^.*$', views.index, name='index'),]
